82b67854394fb3806b746a7ec37915404a4e1277
ariel9930 <arielfaundez@liceovvh.cl>
2026-07-28 15:16:22 -0400

    v

--- STACK_JS/11_funciones_js/static/js/java.js
+++ STACK_JS/11_funciones_js/static/js/java.js
@@ -4,15 +4,17 @@ function saludar() {
     alert(`¡Hola, bienvenido!`);
 }
 
-saludar(); //
-
+//Funcion con parametros
+//El parametro recibe un valor para trabajar en la función.
+//El parametro recibe el tipo de dato al  momento de tomar valor.
 function saludar(nombre) {
     alert("¡Hola, " + nombre + "!");
 }
 
-saludar("Luis");
-saludar("Ana");
+saludar("Ariel");
+saludar("Dantevenitostefano");
 
+function encontrarMayor(){
 function encontrarMaximo(a, b) {
     if (a > b) {
         return a;
@@ -24,4 +26,25 @@ function encontrarMaximo(a, b) {
 let numero1 = 10;
 let numero2 = 7;
 let maximo = encontrarMaximo(numero1, numero2);
-console.log("El número mayor entre", numero1, "y", numero2, "es:", maximo);
+alert(`El número mayor entre ${numero1} y ${numero2}, es: ${maximo}`);
+}
+
+//TAREA
+/*
+Crear una funcion que reciba 3 parametros, a b y c.
+Debe Suma a + b y el resultado restarlo por c.
+Devolver el valor final y mostrar con un alert.
+*/
+function sumaryresta(){
+function sumar(a, b, c){
+    return a + b - c;
+}
+
+let number1 = 10
+let number2 = 2
+let number3 = 4
+
+let sumacion = sumar(number1, number2, number3);
+alert(`La suma entre ${number1}, ${number2} es ${number1 + number2}\ny si le restamos 4 seria: ${sumar} `)
+
+}

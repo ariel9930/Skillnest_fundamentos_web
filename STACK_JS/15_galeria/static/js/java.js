7a66773bf1fd8354321b1824b5b04d9af4e7932d
ariel9930 <arielfaundez@liceovvh.cl>
2026-09-01 09:58:58 -0400

    e

--- STACK_JS/15_galeria/static/js/java.js
+++ STACK_JS/15_galeria/static/js/java.js
@@ -1,8 +1,11 @@
 const foto = document.getElementById("fotoo")
+const letra = document.getElementById("letras")
 
 foto.addEventListener("mouseover", function () {
     foto.src = ("static/img/elgrito.png");
+    letra.innerText = "El grito pintura realizada por Javier Colque"
 });
 foto.addEventListener("mouseout", function () {
     foto.src = "static/img/tungsahur.png"
+    letra.innerHTML = "El gran rey antes de ser derrotado.. (1676-1700)"
 });

8d8ec0ed6cb2df1918d59aa47ebafde3b1bafe43
ariel9930 <arielfaundez@liceovvh.cl>
2026-09-01 11:14:31 -0400

    f

---
+++ STACK_JS/16_seleccionar/static/js/java.js
@@ -0,0 +1,42 @@
+let title = document.querySelector("#title");
+console.log(title); // <h1 id="title">¡Hola, mundo!</h1>
+console.log(`El contenido del titulo es : ${title.textContent}`)
+
+//selecconar un parrafo con la etiqueta
+let parrafo = document.querySelector("p");
+console.log(parrafo);
+
+
+//logo
+let logoImg = document.querySelector(".nav img");
+console.log(logoImg); // <img src="logo.png" alt="logo">
+
+//seleccionar parrafos 
+let parrafos = document.querySelector(".texto");
+console.log(parrafos.textContent); // "Este es el primer párrafo."
+
+//elementos inexistentes
+let boton = document.querySelector("#boton-inexistente");
+console.log(boton); // null
+if (boton !== null) {
+    boton.textContent = "Nuevo Texto";
+} else {
+    console.log("El botón no existe.");
+}
+
+//Tarea: 
+/* Crear un botón y aplicar condiciones al igual que el ejemplo...
+-Debe cambiar su texto al momento de hacerle click
+-Debe actvarse un hover js cambiando el color de fondo */
+const Button = document.getElementById(".btn botonn");
+
+Button.addEventListener("click", function () {
+    console.log("El ratón está sobre el botón");
+    Button.innerText = "tungtung"
+
+});
+
+Button.addEventListener("mouseout", function () {
+    console.log("El ratón ha salido del botón");
+
+});

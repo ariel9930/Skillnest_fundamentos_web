dc0d1f4e41074accbae2a0249df8abf2900c803f
ariel9930 <arielfaundez@liceovvh.cl>
2026-09-01 10:24:22 -0400

    d

---
+++ STACK_JS/12_(interactivdad_con_click)/static/js/script.js
@@ -0,0 +1,37 @@
+document.getElementById("colorButton").addEventListener("click", function () {
+    cambiarColorBody();
+});
+
+document.getElementById("colorButton2").addEventListener("click", function () {
+    cambiarColorCaja1();
+});
+
+document.getElementById("colorButton3").addEventListener("click", function () {
+    cambiarColorCaja2();
+});
+
+
+
+
+
+
+function cambiarColorBody() {
+    let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
+    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    const elemento = document.querySelector("body")
+    document.body.style.backgroundColor = colorAleatorio;
+}
+
+function cambiarColorCaja1() {
+    let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
+    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    const elemento = document.querySelector("#caja1");
+    elemento.style.backgroundColor = colorAleatorio;
+}
+
+function cambiarColorCaja2() {
+    let colores = ["#ff6347", "#66cdaa", "#9370db", "#ffd700", "#4682b4", "#ffa07a"];
+    let colorAleatorio = colores[Math.floor(Math.random() * colores.length)];
+    const elemento = document.querySelector("#caja2");
+    elemento.style.backgroundColor = colorAleatorio;
+}
